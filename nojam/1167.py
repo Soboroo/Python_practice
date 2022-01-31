c0986accd4e8f5764b388e7a455738cf56564491
Soboroo <woo036325@gmail.com>
2022-01-31 13:39:30 +0900

    .

---
+++ nojam/1167.py
@@ -0,0 +1,38 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10**6)
+input = stdin.readline
+
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n):
+    a = list(map(int, input().split()))
+    for i in range(1, len(a), 2):
+        if a[i] == -1:
+            break
+        graph[a[0]].append((a[i], a[i + 1]))
+
+max_distance = 0
+max_distance_node = 0
+
+
+def dfs(node, distance, visited):
+    global max_distance, max_distance_node
+    if distance > max_distance:
+        max_distance = distance
+        max_distance_node = node
+    for i in range(len(graph[node])):
+        if not visited[graph[node][i][0]]:
+            visited[graph[node][i][0]] = True
+            dfs(graph[node][i][0], distance + graph[node][i][1], visited)
+
+
+visited = [False] * (n + 1)
+visited[1] = True
+dfs(1, 0, visited)
+
+visited = [False] * (n + 1)
+visited[max_distance_node] = True
+dfs(max_distance_node, 0, visited)
+
+print(max_distance)
+
---
+++ nojam/24087.py
@@ -0,0 +1,4 @@
+from math import ceil
+a=int(input()) - int(input())
+b=int(input())
+print(250 if a <= 0 else 250+(ceil(a/b)*100))
